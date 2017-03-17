0e6f41c55e996bc3e0b092be6ce9833d94076f74
Syunyo Kawamoto <syunyo@syunyooo-2.local>
2017-03-17 22:35:50 +0900

    リスティング　ナビ　アクティブ設定 ヘルパークラスなど

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	# 以下の関数をどこのファイルからも扱えるようにできるところ。
+
+	# 引数controllerが、現在のcontrollerに含まれていたらtrueを返す。
+	def controller?(controller)
+		controller.include?(params[:controller])
+	end
+
+	# 引数actionが、現在のactionに含まれていたらtrueを返す。
+	def action?(action)
+		action.include?(params[:action])
+	end
 end
